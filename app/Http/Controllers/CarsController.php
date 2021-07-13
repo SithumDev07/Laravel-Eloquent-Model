ee99323ac9fc7fd278b6fc41600a2b12eb445e66
SithumDev07 <sithum.dashantha@gmail.com>
2021-07-13 18:30:37 +0530

    Initial Commit

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Select * from cars;
+
+        $cars = Car::all();
+        //When the user clicked a specific car, it will show the specific car
+        // $cars = Car::where('name', '=', 'AUDI')
+        //     ->get();
+
+        // $cars = Car::where('name', '=', 'AUDI')
+        //     ->firstOrFail();
+
+        //Get data one by one
+        // $cars = Car::chunk(2, function ($cars) {
+        //     foreach ($cars as $car) {
+        //         print_r($car);
+        //     }
+        // });
+
+        // dd($cars); 
+        //Die Dump
+
+        return view('cars.index', [
+            'cars' => $cars
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cars.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Usual Way
+        // $car = new Car();
+        // $car->name = $request->input('name');
+        // $car->founded = $request->input('founded');
+        // $car->description = $request->input('description');
+        // $car->save();
+
+        //Create a new Car
+        $car = Car::create([
+            'name' => $request->input('name'),
+            'founded' => $request->input('founded'),
+            'description' => $request->input('description')
+        ]);
+
+        //When we use Car::make instead of Car::create, we have to call save method
+
+        return redirect('/cars');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $car = Car::find($id);
+
+        // dd($car);
+        return view('cars.edit')->with('car', $car);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $car = Car::where('id', $id)
+            ->update([
+                'name' => $request->input('name'),
+                'founded' => $request->input('founded'),
+                'description' => $request->input('description')
+            ]);
+
+        return redirect('/cars');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    //  Usual Way
+    // public function destroy($id)
+    // {
+    //     $car = Car::find($id);
+
+    //     $car->delete();
+
+    //     return redirect('/cars');
+    // }
+
+    // Better Way
+    public function destroy(Car $car)
+    {
+        $car->delete();
+        return redirect('/cars');
+    }
+}
---
+++ app/Models/Car.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cars';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['name', 'founded', 'description'];
+
+    // protected $timestamps = true;
+
+    //Date Format
+    // protected $dateFormat = 'h:m:s';
+}
